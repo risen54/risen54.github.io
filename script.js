f710adb720bd58485ccd72601825b67946f36867
Satvik <90170287+Risen54@users.noreply.github.com>
2022-05-12 14:04:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let blue = "#4287f5"
+let green = "#42f575"
+let red = "#f55d42"
+let pink = "#f54260"
+let yellow = "#ffab0f"
+let purple = "#6b30b8"
+
+burger = document.querySelector('#burger')
+navphone = document.querySelector('#nav-list-phone')
+
+burger.addEventListener('click', ()=>{
+    navphone.classList.toggle('h-nav');
+    navphone.classList.toggle('h-nav');
+})
